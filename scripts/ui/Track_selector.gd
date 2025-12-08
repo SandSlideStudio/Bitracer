8f5c271a3891270638453f0a1eaf6315dbf65f25
Teun van Wijde <teun@vanwijde.com>
2025-12-08 10:58:43 +0100

    UI change

--- scripts/ui/Track_selector.gd
+++ scripts/ui/Track_selector.gd
@@ -1,51 +1,38 @@
 extends Control
 
-# Add your track scene paths here
-var track_paths := [
-	"res://scenes/Main/Coralus_Iceway+Car.tscn",
+var tracks := [
+	{"name": "Coralus Iceway", "path": "res://scenes/Main/Coralus_Iceway+Car.tscn"}
 ]
 
 var current_index := 0
 
-# Node references
 @onready var track_label = $Panel/VBoxContainer/TrackLabel
 @onready var btn_prev = $Panel/VBoxContainer/HBoxContainer/BtnPrev
 @onready var btn_next = $Panel/VBoxContainer/HBoxContainer/BtnNext
 @onready var btn_play = $Panel/VBoxContainer/BtnPlay
+@onready var sfx_player: AudioStreamPlayer = $SFXPlayer
 
 func _ready():
 	btn_prev.pressed.connect(_on_prev_pressed)
 	btn_next.pressed.connect(_on_next_pressed)
 	btn_play.pressed.connect(_on_play_pressed)
-	
+
 	_update_track_label()
 
-# Update the displayed track name
 func _update_track_label():
-	var path = track_paths[current_index]
-	var file_name = path.get_file().get_basename()
-	track_label.text = file_name
+	track_label.text = tracks[current_index]["name"]
 
-# Navigate previous track
 func _on_prev_pressed():
-	current_index -= 1
-	if current_index < 0:
-		current_index = track_paths.size() - 1
+	current_index = (current_index - 1 + tracks.size()) % tracks.size()
 	_update_track_label()
 
-# Navigate next track
 func _on_next_pressed():
-	current_index += 1
-	if current_index >= track_paths.size():
-		current_index = 0
+	current_index = (current_index + 1) % tracks.size()
 	_update_track_label()
 
-# Load the selected track scene
 func _on_play_pressed():
-	var path = track_paths[current_index]
-	if path != "":
-		get_tree().change_scene_to_file(path)
+	sfx_player.play()
+	get_tree().change_scene_to_file(tracks[current_index]["path"])
 
-# Get currently selected track path (optional for other scripts)
 func get_selected_track_path() -> String:
-	return track_paths[current_index]
+	return tracks[current_index]["path"]
